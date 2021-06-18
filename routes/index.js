8657f4a503ae90831bf006cb969f36813c4b4319
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-06-18 10:11:37 +0200

    card index.ejs updated

--- routes/index.js
+++ routes/index.js
@@ -5,43 +5,44 @@ let items = [
   {
     name: "Bike1",
     price: 550,
-    picture: "bike-1.jpg"
+    picture: "bike-1"
   },
   {
     name: "Bike2",
     price: 750,
-    picture: "bike-2.jpg"
+    picture: "bike-2"
   },
   {
     name: "Bike3",
     price: 1000,
-    picture: "bike-3.jpg"
+    picture: "bike-3"
   },
   {
     name: "Bike4",
     price: 1000,
-    picture: "bike-4.jpg"
+    picture: "bike-4"
   },
   {
     name: "Bike5",
     price: 1000,
-    picture: "bike-5.jpg"
+    picture: "bike-5"
   },
   {
     name: "Bike6",
     price: 1000,
-    picture: "bike-5.jpg"
+    picture: "bike-6"
   },
 ];
 
 /* HOMEPAGE. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {items});
 });
 
 /* SHOP PAGE */
 router.get('/shop', function(req, res, next) {
-  res.render('shop', items);
+  console.log("log items dans route get", items)
+  res.render('shop', {items});
 });
 
 module.exports = router;
